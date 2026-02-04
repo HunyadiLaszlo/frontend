509b2772ff175c7b3b5e8240df8e6d5e926e0082
Hunyadi László <laszlo.hunyadi@umszki.hu>
2026-02-04 20:38:02 +0100

    hiba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { DiakokLista } from './components/diakok-lista/diakok-lista';
 import { DiakSzerkesztes } from './components/diak-szerkesztes/diak-szerkesztes';
 import { DiakLetrehozas } from './components/diak-letrehozas/diak-letrehozas';
+import { Hiba } from './components/hiba/hiba';
 
 export const routes: Routes = [
 { path: '', component: DiakokLista},
 { path: 'diakszerkesztes/:id', component: DiakSzerkesztes},
-{ path: 'diakletrehozas', component: DiakLetrehozas}
+{ path: 'diakletrehozas', component: DiakLetrehozas},
+{ path: '**', component: Hiba }
 ];
